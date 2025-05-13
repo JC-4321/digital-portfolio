2c95f93b54a1baecd85533fb6fa4c0c510975509
JC-WestMec <jesque708@west-mec.org>
2025-05-13 13:04:09 -0700

    finished up with about me stuff

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -22,7 +22,7 @@ export const Home = () => {
 
           <a
             href="#contacts"
-            className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 256, 0.4)]"
+            className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 256, 0.4)] hover:bg-blue-500/10"
           >
             Contact
           </a>
